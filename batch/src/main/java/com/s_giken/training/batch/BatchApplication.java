7bffddf5b9139ba4b10e76a55e374dc038c473cc
Osako Yuka <osako-yuka@s-giken.com>
2024-05-10 08:29:46 +0000

    バッチ処理途中

--- batch/src/main/java/com/s_giken/training/batch/BatchApplication.java
+++ batch/src/main/java/com/s_giken/training/batch/BatchApplication.java
@@ -1,11 +1,16 @@
 package com.s_giken.training.batch;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class BatchApplication implements CommandLineRunner {
@@ -36,6 +41,7 @@ public class BatchApplication implements CommandLineRunner {
 	 * @param args コマンドライン引数
 	 */
 	@Override
+	@Transactional
 	public void run(String... args) throws RuntimeException {
 		logger.info("-".repeat(40));
 
@@ -43,6 +49,37 @@ public class BatchApplication implements CommandLineRunner {
 		// * データベースからデータを取得する
 		// * データを加工する
 		// * 加工したデータをデータベースに登録する
+
+		try {
+			String year = null;
+			String month = null;
+
+			year = args[0].substring(0, 4);
+			month = args[0].substring(4);
+
+			SimpleDateFormat format = new SimpleDateFormat("yyyyMM");
+			Date date = format.parse(args[0]);
+
+			System.out.println(year + "年" + month + "月分の請求情報を確認しています。");
+
+			if (date.equals(this.jdbcTemplate.queryForObject(
+					"SELECT billing_ym FROM T_BILLING_STATUS",
+					Date.class))
+
+					&& (this.jdbcTemplate.queryForObject(
+							"SELECT is_commited FROM T_BILLING_STATUS",
+							boolean.class)) == true) {
+				return;
+			} else {
+				this.jdbcTemplate.update(
+						"DELETE FROM T_BILLING_STATUS WHERE billing_ym = date");
+
+				this.jdbcTemplate.update(
+						"INSERT INTO T_BILLING_STATUS(billing_ym, is_commit) VALUES(date, false)");
+			}
+		} catch (ParseException e) {
+			System.out.println("年月をyyyyMMの文字列で入力してください。");
+		}
 
 		logger.info("-".repeat(40));
 	}
