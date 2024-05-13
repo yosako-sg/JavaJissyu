54f66d3623a11265459f3789add3713add62ece2
Osako Yuka <osako-yuka@s-giken.com>
2024-05-13 07:01:11 +0000

    バッチ処理：if文

--- batch/src/main/java/com/s_giken/training/batch/BatchApplication.java
+++ batch/src/main/java/com/s_giken/training/batch/BatchApplication.java
@@ -1,8 +1,7 @@
 package com.s_giken.training.batch;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,27 +56,47 @@ public class BatchApplication implements CommandLineRunner {
 			year = args[0].substring(0, 4);
 			month = args[0].substring(4);
 
-			SimpleDateFormat format = new SimpleDateFormat("yyyyMM");
-			Date date = format.parse(args[0]);
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+			LocalDate date = LocalDate.parse(args[0], formatter);
 
 			System.out.println(year + "年" + month + "月分の請求情報を確認しています。");
 
 			if (date.equals(this.jdbcTemplate.queryForObject(
-					"SELECT billing_ym FROM T_BILLING_STATUS",
-					Date.class))
-
-					&& (this.jdbcTemplate.queryForObject(
-							"SELECT is_commited FROM T_BILLING_STATUS",
-							boolean.class)) == true) {
+					"SELECT billing_ym FROM T_BILLING_STATUS WHERE is_commited = true",
+					LocalDate.class))) {
 				return;
 			} else {
+				this.jdbcTemplate.execute(
+						"ALTER TABLE T_BILLING_DETAIL_DATA ADD FOREIGN KEY(billing_ym) REFERENCES T_BILLING_STATUS(billing_ym) ON DELETE CASCADE");
+
+				this.jdbcTemplate.execute(
+						"ALTER TABLE T_BILLING__DATA ADD FOREIGN KEY(billing_ym) REFERENCES T_BILLING_STATUS(billing_ym) ON DELETE CASCADE");
+
 				this.jdbcTemplate.update(
 						"DELETE FROM T_BILLING_STATUS WHERE billing_ym = date");
+
+				System.out.println("データベースから" + year + "年" + month + "月分の未確定請求情報を削除しました。");
+
+				System.out.println(year + "年" + month + "請求ステータス情報を追加しています。");
 
 				this.jdbcTemplate.update(
 						"INSERT INTO T_BILLING_STATUS(billing_ym, is_commit) VALUES(date, false)");
 			}
-		} catch (ParseException e) {
+
+			this.jdbcTemplate.execute(
+					"ALTER TABLE T_BILLING_DATA ADD FOREIGN KEY(member_id) REFERENCES T_MEMBER(member_id)");
+
+			this.jdbcTemplate.execute(
+					"ALTER TABLE T_BILLING_DETAIL_DATA ADD FOREIGN KEY(member_id) REFERENCES T_MEMBER(member_id)");
+
+			this.jdbcTemplate.execute(
+					"ALTER TABLE T_BILLING_DETAIL_DATA ADD FOREIGN KEY(charge_id) REFERENCES T_CHARGE(charge_id)");
+
+			this.jdbcTemplate.queryForObject(
+					"SELECT * FROM T_MEMBER WHERE (start_date <= EOMONTH(date)) AND (end_date IS NULL OR end_date >= DATEADD(dd, 1, EOMONTH(date, -1)))",
+					LocalDate.class);
+
+		} catch (IllegalArgumentException e) {
 			System.out.println("年月をyyyyMMの文字列で入力してください。");
 		}
 
